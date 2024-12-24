3b45c1c2f592e8d42d724b1c87692a47949f001d
rodaxx_ <rodaxx30@outlook.com>
2024-12-23 21:26:48 -0300

    add auth endpoints

--- api/chatbot-api.py
+++ api/chatbot-api.py
@@ -1,13 +1,18 @@
 import requests
 import json
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi import FastAPI, HTTPException, Header, WebSocket, WebSocketDisconnect
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import ValidationError
+
 from squeme import WebSocketMessage,BodyGenerate
 from utils import stream,ollam_url,format_ollama_messages,get_context
 from config import UCN_MODEL_NAME
 
+from auth.auth_google import google_callback, google_callback_url
+from auth.jwt_utils import generate_token, verify_token
+from auth.jwt_middleware import JWTMiddleware
 
 app = FastAPI()
 
@@ -19,6 +24,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Proteger las rutas mediante un JWT. Asignar rutas desprotegidas mediante unprotected_routes
+jwt_middleware = JWTMiddleware(unprotected_routes=["/auth/login/google", "/auth/callback/google"])
+app.add_middleware(BaseHTTPMiddleware, dispatch=jwt_middleware)
 
 @app.post("/chat")
 def proxy_request(item: BodyGenerate):
@@ -71,8 +80,30 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         print("Client disconnected")
 
+### Auth
+@app.get("/auth/login/google")
+async def api_google_url():
+    return await google_callback_url()
 
+@app.get("/auth/callback/google")
+async def api_google_callback(code: str):
+    response = await google_callback(code)
+    if response:
+        return response
+    raise HTTPException(status_code=400, detail="Error al validar el codigo de google, ¿estas reutilizando el codigo?")
+ 
+@app.get("/auth/validate")
+async def validate(authorization: str = Header(...)):
+    try:
+        # El middleware ya verifica el token, pero necesitamos obtener el payload para generar el payload del token nuevo.
+        # No es necesario retornar nuevamente la informacion del usuario, guardarla es tarea del frontend.
+        payload = verify_token(authorization)
+        token = generate_token(payload["id"], payload["email"])
+        return { "token": token } 
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Error al validar el token de acceso")
+### End auth
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
